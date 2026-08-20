4d6d8eb88348abff75bc0848ea5e97a7670629bf
Jonathan Schwalbe Öhrström <jonathan@ohrstrom.nu>
2026-08-20 10:25:50 +0200

    Initial commit

---
+++ src/BipBop.cs
@@ -0,0 +1,23 @@
+﻿using McAdminPlugins;
+
+namespace PluginExample;
+
+public class BipBop(IPluginNavigation nav, IServerPluginFiles files) : IPlugin
+{
+    public static IServerPluginFiles? Files { get; private set; }
+    
+    public Task Load()
+    {
+        Files = files;
+        
+        nav.AddPage(
+            text: "Example plugin",
+            href: "/example",
+            order: 10,
+            administratorOnly: true,
+            glyph: "glyph-plugin"
+        );
+        
+        return Task.CompletedTask;
+    }
+}
